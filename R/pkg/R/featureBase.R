6122346ba71b2e4778e3348775656c879b28da41
fenglei <fenglei@wandoujia.com>
2015-05-29 16:45:30 +0800

    .....

--- R/pkg/R/featureBase.R
+++ R/pkg/R/featureBase.R
@@ -7,11 +7,56 @@
 #' @examples
 #'\dontrun{
 #' sc <- sparkR.init()
-#' rdd <- getFeats(sc, c(""))
+#' rdd <- getFeats(sc, "", "", 10)
 #'}
-getFeats <- function(sc, args) {
+getFeats <- function(sc, featList, featTypeList, dateRange, num, seed = -1) {
+
   jrdd <- callJStatic("com.wanodujia.ti.sparkr.FeatBaseService",
-                      "getFeats", sc, args)
+                      "getFeats", sc, featList, dateRange)
+
   # Assume the RDD contains serialized R objects.
-  RDD(jrdd, "byte")
+  # type of element of RDD is charactor vector.
+  orgRDD <- RDD(jrdd, "byte")
+
+  print("running count <- count(orgRDD)...")
+  count <- count(orgRDD)
+  cat("count = ", count)
+
+  print("running takeSample...")
+  udidFeatsList <- list()
+  if (seed == -1) {
+    udidFeatsList <- takeSample(orgRDD, FALSE, as.integer(min(num, count)))
+  } else {
+    udidFeatsList <- takeSample(orgRDD, FALSE, as.integer(min(num, count)), as.integer(seed))
+  }
+  print("takeSample done.")
+
+  print("constructing rdd by parallelize..")
+  sampleRDD <- parallelize(sc, udidFeatsList)
+
+  colTypes <- unlist(strsplit(featTypeList, ','))
+  colCount <- length(colTypes)
+
+  print("running lapply...")
+  typedRDD <- lapply(sampleRDD, function(x) {
+    y <- list()
+    y[1] <- x[1] # udid
+
+    # append typed features
+    for (i in 1 : colCount) {
+        if(colTypes[i] == "string") {
+            y[i + 1] <- x[i + 1]  # + 1 because x[1] is udid
+        } else if (colTypes[i] == "num") {
+            if (x[i + 1] != "") {
+                y[i + 1] <- as.numeric(x[i + 1])
+            } else {
+                y[i + 1] <- 0
+            }
+        }
+    }
+    y
+  })
+  print("lapply done.")
+
+  return(collect(typedRDD))
 }
--- feature-base/src/main/scala/com/wanodujia/ti/sparkr/FeatBaseService.scala
+++ feature-base/src/main/scala/com/wanodujia/ti/sparkr/FeatBaseService.scala
@@ -4,19 +4,20 @@ package com.wanodujia.ti.sparkr
  * @author fegnlei@wandoujia.com, 15-5-25
  */
 
+import java.io.IOException
+
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.{CellUtil, HBaseConfiguration}
-import org.apache.hadoop.hbase.KeyValue.Type
 import org.apache.hadoop.hbase.client.Result
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{CellUtil, HBaseConfiguration}
 import org.apache.spark.api.java.{JavaRDD, JavaSparkContext}
 
 import scala.collection.JavaConverters._
 
 object FeatBaseService {
-  def getConf(tableName: String, fieldsStr: String, date: String): Configuration = {
+  def getConf(tableName: String, fieldsStr: String, dateRange: String): Configuration = {
 
     val conf = HBaseConfiguration.create
 
@@ -32,35 +33,19 @@ object FeatBaseService {
         cols += " " + "data:" + fd
     }
 
+    val dates = dateRange.split(',')
+    val dateStart = dates(0)
+    val dateEnd = dates(1)
+
     conf.set(TableInputFormat.SCAN_COLUMNS, cols)
-    conf.set(TableInputFormat.SCAN_TIMESTAMP, date)
+    conf.set(TableInputFormat.SCAN_TIMERANGE_START, dateStart)
+    conf.set(TableInputFormat.SCAN_TIMERANGE_END, dateEnd)
     conf.set(TableInputFormat.SCAN_CACHEDROWS, "100")
 
     conf
   }
 
-  def test(jsc: JavaSparkContext, args: Array[String]):
-  org.apache.spark.api.java.JavaRDD[Int] = {
-
-    val testRDD = jsc.sc.parallelize(Array(1, 2, 3, 4, 5))
-    JavaRDD.fromRDD(testRDD)
-  }
-
-  def testHBase(jsc: JavaSparkContext, args: Array[String]):
-  org.apache.spark.api.java.JavaRDD[(org.apache.hadoop.hbase.io.ImmutableBytesWritable, org.apache.hadoop.hbase.client.Result)] = {
-    val conf = HBaseConfiguration.create()
-    conf.set(TableInputFormat.INPUT_TABLE, args(0))
-
-    val hBaseRDD = jsc.sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
-      classOf[ImmutableBytesWritable],
-      classOf[Result])
-
-    hBaseRDD.count()
-    JavaRDD.fromRDD(hBaseRDD)
-  }
-
-  // API
-  def getFeats(jsc: JavaSparkContext, conf: Configuration): JavaRDD[String] = {
+  def getFeats(jsc: JavaSparkContext, conf: Configuration): JavaRDD[Array[String]] = {
     val hBaseRDD = jsc.sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
       classOf[ImmutableBytesWritable],
       classOf[Result])
@@ -69,13 +54,12 @@ object FeatBaseService {
     val keyValue = hBaseRDD.map(x => x._2).map(_.list)
 
     //outPut is a RDD[String], in which each line represents a record in HBase
-    val outPut = keyValue.flatMap(x => x.asScala.map(cell =>
-      "columnFamily=%s,qualifier=%s,timestamp=%s,type=%s,value=%s".format(
-        Bytes.toStringBinary(CellUtil.cloneFamily(cell)),
-        Bytes.toStringBinary(CellUtil.cloneQualifier(cell)),
-        cell.getTimestamp.toString,
-        Type.codeToType(cell.getTypeByte),
-        Bytes.toStringBinary(CellUtil.cloneValue(cell))
+    val outPut = keyValue.flatMap(x => x.asScala.map(cell => Array(
+        Bytes.toStringBinary(CellUtil.cloneRow(cell)),        // row key
+        Bytes.toStringBinary(CellUtil.cloneFamily(cell)),     // cf
+        Bytes.toStringBinary(CellUtil.cloneQualifier(cell)),  // cq
+        cell.getTimestamp.toString,                           // ts
+        Bytes.toStringBinary(CellUtil.cloneValue(cell))       // val
       )
     )
     )
@@ -83,8 +67,16 @@ object FeatBaseService {
     JavaRDD.fromRDD(outPut)
   }
 
-  def getFeats(jsc: JavaSparkContext, args: Array[String]):  JavaRDD[String] = {
-    val conf = FeatBaseService.getConf("udid_feat_base", "dl_svr_cnt,dl_clt_cnt,clk_cnt","20150522")
+  //////////////// APIs
+  /**
+   * @param jsc
+   * @param featList
+   * @param dateRange
+   * @return
+   */
+  def getFeats(jsc: JavaSparkContext, featList: String, dateRange: String): JavaRDD[Array[String]] = {
+
+    val conf = FeatBaseService.getConf("udid_feat_base", featList, dateRange)
 
     val featRdd = FeatBaseService.getFeats(jsc, conf)
 
